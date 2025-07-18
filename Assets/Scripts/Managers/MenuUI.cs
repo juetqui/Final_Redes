917302bd1d5f3d7cc8cb14d5841ba5bbeb9834f0
matiasbenavide <matiasbenavide@gmail.com>
2025-07-18 18:21:31 -0300

    Corrijo multiples llamados a boton play

--- Assets/Scripts/Managers/MenuUI.cs
+++ Assets/Scripts/Managers/MenuUI.cs
@@ -1,9 +1,13 @@
 using Fusion;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuUI : NetworkBehaviour
 {
+    [SerializeField] private Button _PlayBtn;
     public void OnClickStartGame()
     {
         NetworkGameManager.Instance.StartSession(isHost: true);
+        _PlayBtn.interactable = false;
     }
 }
