4dccc8900350f294cd61f3dcb202370b50ea9701
Julián Lionel Etchevarne Quimbel <60628743+juetqui@users.noreply.github.com>
2025-07-19 18:58:28 -0300

    cambios

--- Assets/Scripts/Managers/MainMenuHandler.cs
+++ Assets/Scripts/Managers/MainMenuHandler.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,18 +8,18 @@ public class MainMenuHandler : MonoBehaviour
     [Header("Panels")]
     [SerializeField] private GameObject _initialPanel;
     [SerializeField] private GameObject _hostGamePanel;
-    //[SerializeField] private GameObject _statusPanel;
-    //[SerializeField] private GameObject _sessionBrowserPanel;
 
     [Header("Buttons")]
     [SerializeField] private Button _joinLobbyBTN;
     [SerializeField] private Button _hostGameBTN;
-    //[SerializeField] private Button _hostPanelBTN;
 
+
+    //[SerializeField] private GameObject _statusPanel;
+    //[SerializeField] private GameObject _sessionBrowserPanel;
+    //[SerializeField] private Button _hostPanelBTN;
     //[Header("InputFields")] 
     //[SerializeField] private TMP_InputField _sessionName;
     //[SerializeField] private TMP_InputField _nicknameField;
-
     //[Header("Texts")]
     //[SerializeField] private TMP_Text _statusText;
 
@@ -30,8 +27,9 @@ public class MainMenuHandler : MonoBehaviour
     {
         _joinLobbyBTN.onClick.AddListener(Btn_JoinLobby);
         _hostGameBTN.onClick.AddListener(Btn_CreateGameSession);
+        
+        
         //_hostPanelBTN.onClick.AddListener(Btn_ShowHostPanel);
-
         //_networkRunnerHandler.OnJoinedLobby += () =>
         //{
         //    _statusPanel.SetActive(false);
@@ -50,15 +48,15 @@ public class MainMenuHandler : MonoBehaviour
         //_statusText.text = "Joining Lobby...";
     }
 
-    //void Btn_ShowHostPanel()
-    //{
-    //    _sessionBrowserPanel.SetActive(false);
-    //}
-
     void Btn_CreateGameSession()
     {
         _hostGameBTN.interactable = false;
 
         _networkRunnerHandler.CreateGame("GameSession", "Game");
     }
+
+    //void Btn_ShowHostPanel()
+    //{
+    //    _sessionBrowserPanel.SetActive(false);
+    //}
 }
